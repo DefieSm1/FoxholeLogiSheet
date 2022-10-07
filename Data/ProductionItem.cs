f1a55c2615d90c021ea273b99444a8104b7dd2f4
Adam G <adamg99@o2.pl>
2022-10-07 23:15:35 +0200

    Dodaj pliki projektów.

---
+++ Data/ProductionItem.cs
@@ -0,0 +1,61 @@
+﻿namespace FoxholeLogiSheet.Data
+{   
+    public class ProductionItem
+    {
+        public string? Name { get; set; }
+
+        public string? Category { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? DamageType { get; set; }
+
+        public string[]? DamageInfo { get; set; }
+
+        public string? AmmoUsed { get; set; }
+
+        public bool CanMPF { get; set; }
+
+        public int CrateSize { get; set; }
+
+        public int BmatCost { get; set; }
+
+        public int RmatCost { get; set; }
+
+        public int EmatCost { get; set; }
+
+        public int HematCost { get; set; }
+
+        public ProductionItem(string? name, string? category, string? description, string? damageType, string[]? damageInfo, string? ammoUsed, bool canMPF, int crateSize, int bmatCost, int rmatCost, int ematCost, int hematCost)
+        {
+            Name = name;
+            Category = category;
+            Description = description;
+            DamageType = damageType;
+            DamageInfo = damageInfo;
+            AmmoUsed = ammoUsed;
+            CanMPF = canMPF;
+            CrateSize = crateSize;
+            BmatCost = bmatCost;
+            RmatCost = rmatCost;
+            EmatCost = ematCost;
+            HematCost = hematCost;
+        }
+
+        public ProductionItem()
+        {
+            Name = "";
+            Category = "";
+            Description = "";
+            DamageType = "";
+            DamageInfo = new string[] { "" };
+            AmmoUsed = "";
+            CanMPF = true;
+            CrateSize = 0;
+            BmatCost = 0;
+            RmatCost = 0;
+            EmatCost = 0;
+            HematCost = 0;
+        }
+    }
+}
---
+++ Data/ProductionItemCategories.cs
@@ -0,0 +1,23 @@
+﻿namespace FoxholeLogiSheet.Data
+{
+    public class ProductionItemCategories
+    {
+        public Dictionary<string, ProductionItem>? SmallArms { get; set; } = new();
+
+        public Dictionary<string, ProductionItem>? HeavyArms { get; set; } = new();
+
+        public Dictionary<string, ProductionItem>? HeavyMunitions { get; set; } = new();
+
+        public Dictionary<string, ProductionItem>? Utility { get; set; } = new();
+
+        public Dictionary<string, ProductionItem>? Medical { get; set; } = new();
+
+        public Dictionary<string, ProductionItem>? Supplies { get; set; } = new();
+
+        public Dictionary<string, ProductionItem>? Uniforms { get; set; } = new();
+
+        public Dictionary<string, ProductionItem>? Vehicles { get; set; } = new();
+
+        public Dictionary<string, ProductionItem>? Shippables { get; set; } = new();
+    }
+}
