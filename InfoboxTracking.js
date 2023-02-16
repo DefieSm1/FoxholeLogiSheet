50e7f8109902e2211cb1d616d253c071fc8d5745
DefieSm1 <adamg99@o2.pl>
2023-02-16 15:11:31 +0000

    Deploying to gh-pages from  @ 9fa5bdd0835a06fb03abe3ff2b5277be1927c953 🚀

--- InfoboxTracking.js
+++ InfoboxTracking.js
@@ -1,19 +1,17 @@
 ﻿'use strict';
 
-function startInfoboxTracking() {
-    document.addEventListener('mousemove', event => {
-        for (let infobox of document.getElementsByClassName("infobox")) {
-            let itemBottom = infobox.offsetTop + infobox.offsetHeight;
+document.addEventListener('mousemove', event => {
+    for (let infobox of document.getElementsByClassName("infobox")) {
+        let itemBottom = infobox.offsetTop + infobox.offsetHeight;
 
-            if (window.innerHeight - itemBottom < 0 || itemBottom == window.innerHeight)
-                infobox.style.top = window.innerHeight - infobox.offsetHeight + "px";
-            else
-                infobox.style.top = event.y + "px";
+        if (window.innerHeight - itemBottom < 0 || itemBottom == window.innerHeight)
+            infobox.style.top = window.innerHeight - infobox.offsetHeight + "px";
+        else
+            infobox.style.top = event.y + "px";
 
-            if (event.x > window.innerWidth / 2)
-                infobox.style.left = event.x - infobox.offsetWidth - 10 + "px";
-            else
-                infobox.style.left = event.x + 20 + "px";
-            }
-    });
-}
+        if (event.x > window.innerWidth / 2)
+            infobox.style.left = event.x - infobox.offsetWidth - 10 + "px";
+        else
+            infobox.style.left = event.x + 20 + "px";
+        }
+});
