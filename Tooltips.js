1fb4a145278a7e817408427e970458acbcc20b5f
DefieSm1 <adamg99@o2.pl>
2023-10-23 20:54:41 +0000

    Deploying to gh-pages from  @ 2a7dbbd25bb14039a779956d654357a170dbc9e0 🚀

---
+++ Tooltips.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+document.addEventListener('mousemove', event => {
+    for (let tooltip of document.getElementsByClassName("item-tooltip")) {
+        let itemBottom = tooltip.offsetTop + tooltip.offsetHeight;
+
+        if (window.innerHeight - itemBottom < 0 || itemBottom == window.innerHeight)
+            tooltip.style.top = window.innerHeight - tooltip.offsetHeight + "px";
+        else
+            tooltip.style.top = event.y + "px";
+
+        if (event.x > window.innerWidth / 2)
+            tooltip.style.left = event.x - tooltip.offsetWidth - 10 + "px";
+        else
+            tooltip.style.left = event.x + 20 + "px";
+        }
+});
