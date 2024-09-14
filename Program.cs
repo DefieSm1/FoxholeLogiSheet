ffd564c13f1d39ee8d35fb3b8e8007f1c678342e
Adam G <adamg99@o2.pl>
2024-09-14 17:33:20 +0200

    3.1.58.1 Fixed bug that made having mutliple tabs open with different faction gear really screwy.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Blazored.LocalStorage;
+using Blazored.SessionStorage;
 using FoxholeLogiSheet;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredSessionStorage();
 
 await builder.Build().RunAsync();
